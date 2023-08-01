58b8e08a6f075cff8f80ad8ef5ff05e832e3aed1
manish8094 <manishparashar809495@gmail.com>
2023-08-01 18:57:43 +0530

    stating new project on spring boot this project is a new version of my old project medico date :6:52 ,8/01/2023

---
+++ src/main/java/com/manish/medico/MedicoVersion2Application.java
@@ -0,0 +1,13 @@
+package com.manish.medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicoVersion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicoVersion2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/manish/medico/controller/MedicoController.java
@@ -0,0 +1,13 @@
+package com.manish.medico.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MedicoController {
+	@RequestMapping("/abc")
+	public String welcome() {
+		return "welcome";
+
+	}
+}
