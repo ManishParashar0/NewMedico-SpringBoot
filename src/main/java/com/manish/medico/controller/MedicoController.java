859d62bc0928a156a2432b01eee48b63a5087df4
Manish parashar <121606120+ManishParashar0@users.noreply.github.com>
2023-08-15 19:48:39 +0530

    some commit

--- src/main/java/com/manish/medico/controller/MedicoController.java
+++ src/main/java/com/manish/medico/controller/MedicoController.java
@@ -2,6 +2,7 @@ package com.manish.medico.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.manish.medico.Entity.UserEntity;
 
@@ -12,5 +13,12 @@ import lombok.RequiredArgsConstructor;
 public class MedicoController {
 	@Autowired
 	private UserEntity entity;
+
+	@RequestMapping("Usercome")
+	String UserEnter() {
+		// he4rl;oo
+		return null;
+
+	}
 
 }
