1663be21d52ffce153331bb3b7de482954e7205d
Manish parashar <121606120+ManishParashar0@users.noreply.github.com>
2023-08-18 19:15:45 +0530

    change

--- src/main/java/com/manish/medico/Repo/Entity_Repo.java
+++ src/main/java/com/manish/medico/Repo/Entity_Repo.java
@@ -6,4 +6,5 @@ import com.manish.medico.Entity.UserEntity;
 
 public interface Entity_Repo extends JpaRepository<UserEntity, Integer> {
 	UserEntity findbyuseremail(String email);
+//	UserEntity findbyuseremail(String email);
 }
